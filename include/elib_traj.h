887920c5587490b2117786a940a056d508700d56
yanan.chen <yanan.chen8@gientech.com>
2026-05-13 15:21:15 +0800

    feat: add umbrella header

---
+++ include/elib_traj.h
@@ -0,0 +1,9 @@
+/* elib_traj.h - Trajectory Planner Main Header */
+#ifndef ELIB_TRAJ_H
+#define ELIB_TRAJ_H
+
+#include "elib_traj_defs.h"
+#include "elib_traj_trapezoid.h"
+#include "elib_traj_scurve.h"
+
+#endif /* ELIB_TRAJ_H */
